d45776ba70f693269946797e01020d96bfdc8d80
truongnguyen-kt <ngtruong166@gmail.com>
2026-04-24 11:05:31 +0700

    input & output

---
+++ CORE/02_Input_Output/src/Input_Output.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input_Output {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println("The number is: " + number);
+    }
+}
